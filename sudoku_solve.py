c6d7e606ec3452b664993d2f59cf3a79de3d8366
gomazarashi <Seto42357@gmail.com>
2023-07-14 11:34:30 +0900

    [add]read_sudoku関数を追加

--- sudoku_solve.py
+++ sudoku_solve.py
@@ -1,13 +1,19 @@
 import numpy as np
 
+#csv形式の数独を読み込み、空白部分は0としてnumpy配列に格納して返す関数
+def read_sudoku(filename):
+    sudoku = np.genfromtxt(filename, delimiter=',', dtype=int,filling_values=0)#欠損値(空白)を含むcsvファイルを読み込み、欠損値を0としてnumpy配列に格納
+    return sudoku
+
 #最初の段階で空白であるセルを探す関数
 def find_blank_cell_by_initial_state(sudoku):
     return(list(zip(*np.where(sudoku == 0))))
 
 def main():
-    sudoku=np.zeros((9,9),dtype=int)
-    unsolved_cells_tf=[[cell,True] for cell in list(find_blank_cell_by_initial_state(sudoku))] #まだ解かれていないセルのリスト 数字が0でなければTrue
-    print(unsolved_cells_tf)
+    sudoku=read_sudoku("sudoku_sample.csv")
+    print(sudoku)
+    """unsolved_cells_tf=[[cell,True] for cell in list(find_blank_cell_by_initial_state(sudoku))] #まだ解かれていないセルのリスト 数字が0でなければTrue
+    print(unsolved_cells_tf)"""
 
 
 if __name__ == '__main__':
